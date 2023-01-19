c68db85c7ee855a518da755c7824adcccea8defa
professorjrod <73684895+professorjrod@users.noreply.github.com>
2023-01-19 12:06:28 -0800

    Include actual result in error message

--- wasm.rb
+++ wasm.rb
@@ -4,14 +4,14 @@ def main
 
   actual = SExpressionParser.new.parse(input)
 
-  raise unless actual == expected
+  raise actual.inspect unless actual == expected
 
   input = '(test)'
   output = ['test']
 
   actual = SExpressionParser.new.parse(input)
 
-  raise unless actual == output
+  raise actual.inspect unless actual == output
 
   return if ARGV.empty?
 
@@ -23,10 +23,32 @@ end
 class SExpressionParser
   def parse(string)
     self.string = string
+    parse_expression
+  end
+
+  def parse_expression
+    if can_read?(/\(/)
+      expressions = []
+      read(/\(/)
+      expressions << parse_expression
+      read(/\)/)
+      expressions
+    else
+      read(/[^)]+/)
+    end
   end
 
   private
 
+  def can_read?(pattern)
+    /\A#{pattern}/.match?(string)
+  end
+
+  def read(pattern)
+    match = /\A#{pattern}/.match(string) or raise "Expected #{pattern} but got #{string}"
+    self.string = match.post_match
+    match.to_s
+  end
   attr_accessor :string
 end
 
